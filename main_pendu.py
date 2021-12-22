27d42c67c2d070d2c1325dcc54c42e63b9f93f82
wabou <wahibou1108@hotmail.fr>
2021-12-22 07:57:20 +0100

    quelques ajustements

--- main_pendu.py
+++ main_pendu.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 # fichier main_pendu.py
 
 import module_samia as ms
@@ -6,9 +7,38 @@ import module_wahib as mw
 import module_jarfar as mj
 import module_sikou as mk
 
+import tkinter as tk
 
 
 
+
+# ------------------------- DÉBUT ------------------------
+
+racine = tk.Tk()
+racine.title("Jeux du pendu sur Tkinter")
+racine.iconbitmap("src/pendu.ico")
+racine.geometry("800x600")
+racine.resizable(width=False, height=False)    # Pour interdire le redimentionnement en largeur ou en hauteur
+
+
+#racine.positionfrom("user")
+#racine.sizefrom("user")
+#racine.geometry("800x600+300+300")
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------- FIN ------------------------
+
+racine.mainloop()
 
 
 
