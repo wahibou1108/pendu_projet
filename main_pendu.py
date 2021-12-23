df4c4c687d0c8b946261d0ff61cebb00dd0707f7
wabou <wahibou1108@hotmail.fr>
2021-12-23 15:37:32 +0100

    petite mise à jour du .gitignore et de main_pendu.py

--- main_pendu.py
+++ main_pendu.py
@@ -1,22 +1,62 @@
 #coding:utf-8
 # fichier main_pendu.py
 
+'''
+ZONE DE TEST FONCTION ENTRE LES LIGNES 15 ET 40 :
+________________________________________________
+
+MAIS PENSER A LES INTEGRER PAR LA SUITE A VOS MODULE RESPECTIFS
+ET LES IMPORTER VIA LA NOTATION POINTEE COMME DANS L'EXEMPLE :
+https://docs.google.com/drawings/d/11fcRO11Jpc06jaaqctlZfIPX_ASjFkrGnvvL_YCMnEU/edit
+CECI AFIN DE GARDER UN CODE AUSSI LISIBLE QUE POSSIBLE
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------ LIGNE 40 ----------------------
+# -------- J'IMPORTE CHAQUE MODULE DE MA SCRUM -----------
+
 import module_samia as ms
 import module_abder as ma
 import module_wahib as mw
 import module_jarfar as mj
 import module_sikou as mk
 
+
+# ------------------ J'IMPORTE TKINTER -------------------
+
 import tkinter as tk
 
-
-
-
 # ------------------------- DÉBUT ------------------------
+# ----------------- TKINTER COMMENCE ICI -----------------
 
 racine = tk.Tk()
 racine.title("Jeux du pendu sur Tkinter")
-racine.iconbitmap("src/pendu.ico")
+racine.iconbitmap("src/IcoPendu9.ico")
 racine.geometry("800x600")
 racine.resizable(width=False, height=False)    # Pour interdire le redimentionnement en largeur ou en hauteur
 
@@ -36,8 +76,34 @@ racine.resizable(width=False, height=False)    # Pour interdire le redimentionne
 
 
 
-# ------------------------- FIN ------------------------
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------- FIN ------------------------
 racine.mainloop()
 
 
